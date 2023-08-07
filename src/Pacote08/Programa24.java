41c79c2dfdc4f5430a4611297e1ccc40f8f23917
Wallace Mateus <134143717+WallaceM2@users.noreply.github.com>
2023-08-07 10:25:58 -0300

    Organizando Pacotes/Codigos

---
+++ src/Pacote08/Programa24.java
@@ -0,0 +1,19 @@
+package Pacote08;
+
+//Pacotes
+
+/*
+ * Pacotes servem para organizar o codigo fonte do projeto;
+ * Pacotes indicam onde determinado arquivo do projeto se encontra;
+ * Os nomes de pacotes devem ser em letras minusculas, sem espaço, sem caractere especial, sem acentuação;
+ * Por convenção, é utilizado nos nomes dos pacotes, o dominio da empresa criadora do projeto;
+ * ao contrario, sendo a ultima parte do nome o especificador do pacote;
+ * 
+ * - Padrao de PROJETO;
+ * - MVC - Model - View - Controller;
+ */
+public class Programa24 {
+    public static void main(String[] args) {
+        
+    }
+}
